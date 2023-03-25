adb7f60c223fdf991ff3cd3c9ca5b9e9ea8d0373
Lambotharan Logendran <95412261+lambooo1@users.noreply.github.com>
2023-03-25 23:02:20 +0100

    Update ytDownloader.py

--- ytDownloader.py
+++ ytDownloader.py
@@ -2,24 +2,15 @@ from pytube import YouTube
 from sys import argv
 from pathlib import Path
 
-
 link = argv[1]
-#yt = YouTube(link)
 
-#wichtig link ändern, bei neuem Video!!
+#change link for new videos!
 yt = YouTube('https://www.youtube.com/watch?v=iogcY_4xGjo') 
 
 print("Title :", yt.title)
-
 print("View: ", yt.views)
 
 yd = yt.streams.get_highest_resolution()
-yd.download('C:\\Users\\Lambo\\OneDrive - Bildungszentrum Zürichsee\\Dokumente\\Privat\\Projekte\\ytDownloads')
+yd.download('Path for downloaded videos')
 
-#yd = yt.streams.first()
-#yd.download('C:\\Users\\Lambo\\OneDrive - Bildungszentrum Zürichsee\\Dokumente\\Privat\\Projekte\\ytDownloads')
-
-#in Terminal: python ytDownloader.py "https://www.youtube.com/watch?v=vEQ8CXFWLZU&t=68"
-#python ytDownloader.py "https://www.youtube.com/@InternetMadeCoder"
-#python ytDownloader.py "https://youtu.be/lp0Sxn42TGs"
-#python ytDownloader.py "https://www.youtube.com/watch?v=iogcY_4xGjo"
+#terminal: python ytDownloader.py "https://www.youtube.com/watch?v=iogcY_4xGjo"
